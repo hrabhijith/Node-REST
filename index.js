e4998ad7d091549eddd2934bb56c0cf1b57a7db7
Abhijith <hrabhijith@gmail.com>
2019-05-31 01:48:18 +0530

    Started checks

--- index.js
+++ index.js
@@ -14,28 +14,28 @@ var handlers = require('./lib/handlers');
 // });
 
 var httpServer = http.createServer((req, res) => {
-   unifiedFunction(req,res);
+    unifiedFunction(req, res);
 
 });
 
 httpServer.listen(config.httpPort, () => {
-    console.log("Server is listening on the port " , config.httpPort +" with environment " , config.envName);
+    console.log("Server is listening on the port ", config.httpPort + " with environment ", config.envName);
 });
 
-var httpsServerOptions ={
-'key' : fs.readFileSync('./https/key.pem'),
-'cert' : fs.readFileSync('./https/cert.pem')
+var httpsServerOptions = {
+    'key': fs.readFileSync('./https/key.pem'),
+    'cert': fs.readFileSync('./https/cert.pem')
 };
 
-var httpsServer = https.createServer(httpsServerOptions,(req, res) => {
-    unifiedFunction(req,res);
- 
- });
- 
- httpsServer.listen(config.httpsPort, () => {
-     console.log("Server is listening on the port " , config.httpsPort +" with environment " , config.envName);
- })
-var unifiedFunction = function(req,res) {
+var httpsServer = https.createServer(httpsServerOptions, (req, res) => {
+    unifiedFunction(req, res);
+
+});
+
+httpsServer.listen(config.httpsPort, () => {
+    console.log("Server is listening on the port ", config.httpsPort + " with environment ", config.envName);
+})
+var unifiedFunction = function (req, res) {
     var parserUrl = url.parse(req.url, true);
     var path = parserUrl.pathname;
     var trimmedPath = path.replace(/^\/+|\/+$/g, '');
@@ -49,34 +49,35 @@ var unifiedFunction = function(req,res) {
     })
     req.on('end', () => {
         buffer += decoder.end();
-        var chosenHandler = typeof(router[trimmedPath])!= 'undefined'? router[trimmedPath] : handlers.notFound ;
+        var chosenHandler = typeof (router[trimmedPath]) != 'undefined' ? router[trimmedPath] : handlers.notFound;
         var data = {
-            'trimmedPath' : trimmedPath,
+            'trimmedPath': trimmedPath,
             'queryStringObject': queryStringObject,
             'method': method,
             'headers': headers,
             'payload': helpers.parseJsonToObject(buffer)
         };
 
-        chosenHandler(data, function (statusCode, payLoad)  {
-             statusCode = typeof(statusCode) == 'number'? statusCode : 200;
-             
-             payLoad = typeof(payLoad) == 'object'? payLoad: {};
+        chosenHandler(data, function (statusCode, payLoad) {
+            statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
+
+            payLoad = typeof (payLoad) == 'object' ? payLoad : {};
             var payLoadString = JSON.stringify(payLoad);
             res.setHeader('content-type', 'application/json');
             res.writeHead(statusCode);
             res.end(payLoadString);
-           // console.log("Parsed url is " + trimmedPath + ' with method ' + method + ' with query ', queryStringObject ,
-           // ' with headers ', headers , ' with payload ' + buffer);
-           //console.log("Returned route is ",statusCode, payLoad);
+            // console.log("Parsed url is " + trimmedPath + ' with method ' + method + ' with query ', queryStringObject ,
+            // ' with headers ', headers , ' with payload ' + buffer);
+            //console.log("Returned route is ",statusCode, payLoad);
         });
-        
-        
+
+
     })
 };
 
 var router = {
     'users': handlers.users,
     'ping': handlers.ping,
-    'tokens': handlers.tokens
+    'tokens': handlers.tokens,
+    'checks': handlers.checks
 };
